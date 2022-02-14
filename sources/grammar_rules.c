1221d09a0811b9dd84739024d07a505219f82d25
FaustoFaggion <fausto.faggion@hotmail.com>
2022-02-14 16:04:23 -0300

    parsing should be ok. Wornkin on grammar rules

---
+++ sources/grammar_rules.c
@@ -0,0 +1,8 @@
+#include "minishell.h"
+/*
+void	grammar_rules(t_cmd *cmd, int i)
+{
+	if (cmd->line[i] == '|')
+
+}
+*/
--- sources/parse_cmd_tab_malloc.c
+++ sources/parse_cmd_tab_malloc.c
@@ -6,7 +6,7 @@
 /*   By: fagiusep <fagiusep@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 13:05:09 by fagiusep          #+#    #+#             */
-/*   Updated: 2022/02/14 14:32:06 by fagiusep         ###   ########.fr       */
+/*   Updated: 2022/02/14 16:01:39 by fagiusep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 static void	malloc_char(t_cmd *cmd)
 {
+	if (cmd->flag == 1)
+	{
+		cmd->y = 0;
+		cmd->tab_y = (char **)malloc(((sizeof(char *)) * (cmd->size_tab_y + 1)));
+		if (!cmd->tab_y)
+			return ;
+	}
 	cmd->len_ptr = 0;
 	cmd->len_ptr = parse_cmd_tab_len(cmd, cmd->z);
 	cmd->tab_y[cmd->y] = ft_substr(cmd->line, cmd->z, cmd->len_ptr);
@@ -24,52 +31,37 @@ static void	malloc_char(t_cmd *cmd)
 	cmd->flag = 0;
 }
 
-static void	malloc_metachar(t_cmd *cmd)
+static void	close_malloc(t_cmd *cmd)
 {
-	if (cmd->flag == 0)
-	{
-		while (cmd->y < cmd->size_tab_y)
+	while (cmd->y < cmd->size_tab_y)
 		{
 			cmd->tab_y[cmd->y] = NULL;
 			cmd->y++;
 		}
 		cmd->tab_x[cmd->x] = cmd->tab_y;
-		cmd->tab_y = (char **)malloc(((sizeof(char *)) * (cmd->size_tab_y + 1)));
-		if (cmd->tab_y == NULL)
-			free_tab(cmd->tab_y, cmd->size_tab_y);
 		cmd->x++;
-		cmd->y = 0;
-	}
+}
+static void	malloc_metachar(t_cmd *cmd)
+{
+	if (cmd->flag == 0)
+		close_malloc(cmd);
+	cmd->tab_y = (char **)malloc(((sizeof(char *)) * (cmd->size_tab_y + 1)));
+	if (cmd->tab_y == NULL)
+		free_tab(cmd->tab_y, cmd->size_tab_y);
+	cmd->y = 0;
 	parse_cmd_tab_len(cmd, cmd->z);
 	cmd->tab_y[cmd->y] = ft_substr(cmd->line, cmd->z, cmd->len_ptr);
 	if (cmd->tab_y[cmd->y] == NULL)
 		free_tab(cmd->tab_y, cmd->size_tab_y);
 	cmd->y++;
-	while (cmd->y < cmd->size_tab_y)
-	{
-		cmd->tab_y[cmd->y] = NULL;
-		cmd->y++;
-	}
-	cmd->tab_x[cmd->x] = cmd->tab_y;
-}
-
-static void	malloc_prepare_next(t_cmd *cmd)
-{
+	close_malloc(cmd);
 	cmd->z = cmd->z + cmd->len_ptr;
-	cmd->tab_y = (char **)malloc(((sizeof(char *)) * (cmd->size_tab_y + 1)));
-	if (cmd->line[cmd->z] == '\0')
-	{
-		free(cmd->tab_y);
-		cmd->tab_y = NULL;
-	}
-	cmd->y = 0;
 	cmd->flag = 1;
-	cmd->x++;
 }
 
 static void	malloc_finish(t_cmd *cmd)
 {
-	if (cmd->tab_y != NULL)
+	if (cmd->flag == 0)
 	{
 		while (cmd->y < cmd->size_tab_y)
 		{
@@ -80,10 +72,7 @@ static void	malloc_finish(t_cmd *cmd)
 		cmd->tab_x[cmd->x + 1] = NULL;
 	}
 	else
-	{
-		free(cmd->tab_y);
 		cmd->tab_x[cmd->x] = NULL;
-	}
 }
 
 char	**parse_cmd_tab_malloc(t_cmd *cmd)
@@ -93,19 +82,16 @@ char	**parse_cmd_tab_malloc(t_cmd *cmd)
 	cmd->z = 0;
 	cmd->flag = 1;
 	cmd->len_ptr = 0;
-	cmd->tab_y = (char **)malloc(((sizeof(char *)) * (cmd->size_tab_y + 1)));
-	if (!cmd->tab_y)
-		return (NULL);
 	while (cmd->line[cmd->z] != '\0')
 	{
 		while (cmd->line[cmd->z] == ' ' && cmd->line[cmd->z] != '\0')
 			cmd->z++;
+//		grammar_rules(cmd, cmd->z);
 		if (ft_strchr("|<>", cmd->line[cmd->z]) == NULL)
 			malloc_char(cmd);
 		else
 		{
 			malloc_metachar(cmd);
-			malloc_prepare_next(cmd);
 			if (!cmd->tab_x)
 				break ;
 		}
