4dac6bbf74c1b01cf2cda06afdff6fa3a4765f2d
FaustoFaggion <fausto.faggion@hotmail.com>
2022-03-26 10:25:09 -0300

    problems with char*** malloc solved. NO leaks

--- sources/cmd_tab.c
+++ sources/cmd_tab.c
@@ -6,19 +6,84 @@
 /*   By: fagiusep <fagiusep@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:51:10 by fagiusep          #+#    #+#             */
-/*   Updated: 2022/03/08 17:42:21 by fagiusep         ###   ########.fr       */
+/*   Updated: 2022/03/26 10:10:50 by fagiusep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+/*
+static void cmd_count(t_tkn *tkn)
+{
+	int	i;
+	int	pipe;
+	int	redirect;
+	
+	tkn->cmd_count = 0;
+	pipe = 0;
+	redirect = 0;
+	i = -1;
+	while (++i < tkn->tkn_count)
+	{
+		if (ft_strncmp(tkn->lexemas[i], "PIPE", 4) == 0)
+			pipe++;
+		else if (ft_strncmp(tkn->lexemas[i], "WORD", 4) != 0
+			&& ft_strncmp(tkn->lexemas[i], "ASSIGNMENT_WORD", 15) != 0)
+			redirect++;
+		tkn->cmd_count = (pipe +(pipe + 1)) + redirect;
+	}
+}
 
+void	cmd_tab(t_tkn *tkn)
+{
+	int	i;
+	int	j;
+	
+	cmd_count(tkn);
+	tkn->cmd = (char ***)malloc((tkn->cmd_count + 1) * (sizeof(char **)));
+	if (!tkn->cmd)
+		return ;
+	tkn->cmd_lex = (char ***)malloc((tkn->cmd_count + 1) * (sizeof(char **)));
+	if (!tkn->cmd_lex)
+		return ;
+	i = 0;
+	j = 0;
+	while (j < tkn->tkn_count)
+	{
+		if (ft_strncmp(tkn->lexemas[j], "WORD", 4) == 0)
+		{
+			tkn->cmd[i] = &tkn->tokens[j];
+			tkn->cmd_lex[i] = &tkn->lexemas[i];
+			while  (ft_strncmp(tkn->lexemas[i], "WORD", 4) != 0
+				&& ft_strncmp(tkn->lexemas[i], "ASSIGNMENT_WORD", 15) != 0)
+				j++;
+			i++;
+		}
+		else if (ft_strncmp(tkn->lexemas[i], "PIPE", 4) == 0)
+		{
+			tkn->cmd[i] = &tkn->tokens[i];
+			tkn->cmd_lex[i] = &tkn->lexemas[i];
+			j++;
+			i++;
+		}
+		else
+		{
+			tkn->cmd[i] = &tkn->tokens[i];
+			tkn->cmd_lex[i] = &tkn->lexemas[i];
+			j = j + 2;
+			i++;
+		}
+	}
+	
+	tkn->cmd[tkn->cmd_count] = NULL;
+}
+*/
 static int	cmd_metachar(t_tkn *tkn, int i)
 {
 	int		count;
 	int		j;
 	char	**temp;
 	char	**temp_lex;
-////////
+
 	count = 0;
 	if (ft_strncmp(tkn->lexemas[i], "PIPE", 4) == 0)
 		count = 1;
@@ -86,25 +151,31 @@ static int	cmd_word(t_tkn *tkn, int i)
 
 void	cmd_tab(t_tkn *tkn)
 {
-	int		i;
-	int		count;
-
+	int	i;
+	int	count;
+	int	pipe;
+	int redirect;
+	
 	tkn->cmd_count = 0;
 	count = 0;
+	pipe = 0;
+	redirect = 0;
 	i = -1;
 	while (++i < tkn->tkn_count)
 	{
-		if (ft_strncmp(tkn->lexemas[i], "WORD", 4) != 0
+		if (ft_strncmp(tkn->lexemas[i], "PIPE", 4) == 0)
+			pipe++;
+		else if (ft_strncmp(tkn->lexemas[i], "WORD", 4) != 0
 			&& ft_strncmp(tkn->lexemas[i], "ASSIGNMENT_WORD", 15) != 0)
-			count = count + 3;
-		count++;
+			redirect++;
 	}
-	tkn->cmd = (char ***)malloc((sizeof(char **) * sizeof(tkn->tokens) * 2)
-			+ (sizeof(char **) * 2));
+	count = (pipe +(pipe + 1)) + redirect;
+	tkn->cmd = (char ***)malloc(((tkn->tkn_count * sizeof(char *))
+		+ (count * sizeof(char *))) + 1);
 	if (!tkn->cmd)
 		return ;
-	tkn->cmd_lex = (char ***)malloc((sizeof(char **) * sizeof(tkn->tokens) * 2)
-			+ (sizeof(char **) * 2));
+	tkn->cmd_lex = (char ***)malloc(((tkn->tkn_count * sizeof(char *))
+		+ (count * sizeof(char *))) + 1);
 	if (!tkn->cmd)
 		return ;
 	i = 0;
--- sources/exec_cmd_unset.c
+++ sources/exec_cmd_unset.c
@@ -1,12 +1,35 @@
 #include "minishell.h"
 
+static void	unset_var(t_tkn *tkn, int i)
+{
+	char	**temp;
+	int		x;
+	int		y;
+
+	temp = (char **)malloc((tkn->envp_count) * sizeof(char *));
+	x = 0;
+	y = 0;
+	while (tkn->envp[x + y] != NULL)
+	{
+		if (ft_strncmp(tkn->envp[x], tkn->cmd[i][1], ft_strlen(tkn->cmd[i][1])) == 0)
+			y++;
+		else
+		{
+			temp[x] = ft_strdup(tkn->envp[x + y]);
+			x++;
+		}
+	}
+	temp[x] = NULL;
+	free_tab(&tkn->envp, 2);
+	tkn->envp = (char **)malloc((tkn->envp_count) * sizeof(char *));
+	tkn->envp = temp;
+	tkn->envp_count--;
+}
 
 void	exec_cmd_unset(t_tkn *tkn, int i)
 {
 	int		x;
 	int		flag;
-	int		y;
-	char	**temp;
 
 	flag = 0;
 	x = 0;
@@ -17,23 +40,5 @@ void	exec_cmd_unset(t_tkn *tkn, int i)
 		x++;
 	}
 	if (flag == 1)
-	{
-		temp = (char **)malloc((tkn->envp_count) * sizeof(char *));
-		x = 0;
-		y = 0;
-		while (tkn->envp[x + y] != NULL)
-		{
-			if (ft_strncmp(tkn->envp[x], tkn->cmd[i][1], ft_strlen(tkn->cmd[i][1])) != 0)
-				y++;
-			else
-			{
-				temp[x] = ft_strdup(tkn->envp[x + y]);
-				x++;
-			}
-		}
-		temp[x] = NULL;
-		free_tab(&tkn->envp, 2);
-		tkn->envp = temp;
-		tkn->envp_count--;
-	}
+		unset_var(tkn, i);
 }	
