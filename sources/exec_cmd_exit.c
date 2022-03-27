68719c05a80a9fa551c445852c8645cffc6e9f03
FaustoFaggion <fausto.faggion@hotmail.com>
2022-03-27 08:55:04 -0300

    exit built_in done. Treat signals has to be done next

--- sources/exec_cmd_exit.c
+++ sources/exec_cmd_exit.c
@@ -19,48 +19,51 @@ static int	cmd_digit(t_tkn *tkn, int i)
 	flag = 0;
 	while (tkn->cmd[i][1][j] != '\0')
 	{
-		if (ft_isdigit(tkn->cmd[i][1][j]) == 1)
-		{
+		if (j == 0 && tkn->cmd[i][1][j] != '+' && tkn->cmd[i][1][j] != '-'
+					&& ft_isdigit(tkn->cmd[i][1][j]) == 1)
 			flag = 1;
-		}
+		if (j != 0 && ft_isdigit(tkn->cmd[i][1][j]) == 1)
+			flag = 1;
 		j++;
 	}
 
 	return (flag);
 }
 
-void	exec_cmd_exit(t_tkn *tkn, int i)
+static int	not_valid_exit(t_tkn *tkn, int i, int len, int is_digit)
 {
-	int	len;
-	int	is_digit;
-	int	ret;
-
-	len = cmd_size(tkn, i);
-	printf("len %d", len);
-	is_digit = cmd_digit(tkn, i);
 	if (is_digit == 1)
 	{
 		ft_putendl_fd("exit:\nexit: ", 2);
 		ft_putendl_fd(tkn->cmd[i][1], 2);
 		ft_putendl_fd(": requer argumento numérico\n", 2);
-		return ;
+		tkn->exit_signal = 2;
+		return (1);
 	}
 	else if (len > 2)
 	{
 		ft_putendl_fd("exit\nbash: exit: número excessivo de argumentos\n", 2);
-		tkn->exit_signal = 2
-		return ;
+		tkn->exit_signal = 1;
+		return (1);
 	}
+	return (0);
+}
+
+void	exec_cmd_exit(t_tkn *tkn, int i)
+{
+	int	len;
+	int	is_digit;
+
+	len = cmd_size(tkn, i);
+	is_digit = cmd_digit(tkn, i);
+	if (not_valid_exit(tkn, i, len, is_digit) == 1)
+		return;
 	if (tkn->cmd[i][1] == NULL)
-		ret = 0;
+		tkn->exit_signal = 0;
 	else
-		ret = ft_atoi(tkn->cmd[i][1]);
-	
+		tkn->exit_signal = (ft_atoi(tkn->cmd[i][1]) % 256);
 	exit_shell(tkn);
 	free_tab(&tkn->envp, tkn->envp_count);
 	rl_clear_history();
-	exit(ret);
-
-
-
+	exit(tkn->exit_signal);
 }
